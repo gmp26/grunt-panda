70e9d14653392620bcd46030825e5f1ccb8cf9d0
gmp26 <gmp26@cam.ac.uk>
2013-06-08 09:04:56 +0100

    tests now use noramlised pathnames for Windows compatibility

--- test/panda_test.js
+++ test/panda_test.js
@@ -1,7 +1,8 @@
 (function(){
   "use strict";
-  var grunt;
+  var grunt, path;
   grunt = require("grunt");
+  path = require("path");
   exports.panda = {
     setUp: function(done){
       return done();
@@ -9,38 +10,38 @@
     test1: function(test){
       var actual, expected;
       test.expect(1);
-      actual = grunt.file.read("tmp/test1.html");
-      expected = grunt.file.read("test/expected/test1.html");
+      actual = grunt.file.read(path.normalize("tmp/test1.html"));
+      expected = grunt.file.read(path.normalize("test/expected/test1.html"));
       test.equal(actual, expected, "markdown should compile to html");
       return test.done();
     },
     test2: function(test){
       var actual, expected;
       test.expect(1);
-      actual = grunt.file.read("tmp/test2.html");
-      expected = grunt.file.read("test/expected/test2.html");
+      actual = grunt.file.read(path.normalize("tmp/test2.html"));
+      expected = grunt.file.read(path.normalize("test/expected/test2.html"));
       test.equal(actual, expected, "multiple inputs should concatenate");
       return test.done();
     },
     test3: function(test){
       var exists, actual4, expected4;
       test.expect(3);
-      exists = grunt.file.exists("tmp/test3.pdf");
+      exists = grunt.file.exists(path.normalize("tmp/test3.pdf"));
       test.ok(exists, "it should create a pdf");
-      actual4 = grunt.file.read("tmp/test4.html");
-      expected4 = grunt.file.read("test/expected/test4.html");
+      actual4 = grunt.file.read(path.normalize("tmp/test4.html"));
+      expected4 = grunt.file.read(path.normalize("test/expected/test4.html"));
       test.equal(actual4, expected4, "it should create multiple outputs");
-      exists = grunt.file.exists("tmp/test5.docx");
+      exists = grunt.file.exists(path.normalize("tmp/test5.docx"));
       test.ok(exists, "it should create a docx");
       return test.done();
     },
     test4: function(test){
       var exists, actual, expected;
       test.expect(2);
-      exists = grunt.file.isDir("tmp/fixtures");
+      exists = grunt.file.isDir(path.normalize("tmp/fixtures"));
       test.ok(exists, "it should create an output directory");
-      actual = grunt.file.read("tmp/fixtures/input.html");
-      expected = grunt.file.read("test/expected/fixtures/input.html");
+      actual = grunt.file.read(path.normalize("tmp/fixtures/input.html"));
+      expected = grunt.file.read(path.normalize("test/expected/fixtures/input.html"));
       test.equal(actual, expected, "it should create html for each input");
       return test.done();
     }
--- test/panda_test.ls
+++ test/panda_test.ls
@@ -1,7 +1,6 @@
 "use strict"
-grunt = require("grunt")
-
-
+grunt = require "grunt"
+path = require "path"
 
 #
 #  ======== A Handy Little Nodeunit Reference ========
@@ -29,40 +28,40 @@ exports.panda =
 
   test1: (test) ->
     test.expect 1
-    actual = grunt.file.read("tmp/test1.html")
-    expected = grunt.file.read("test/expected/test1.html")
+    actual = grunt.file.read path.normalize "tmp/test1.html"
+    expected = grunt.file.read path.normalize "test/expected/test1.html"
     test.equal actual, expected, "markdown should compile to html"
     test.done()
 
   
   test2: (test) ->
     test.expect 1
-    actual = grunt.file.read("tmp/test2.html")
-    expected = grunt.file.read("test/expected/test2.html")
+    actual = grunt.file.read path.normalize "tmp/test2.html"
+    expected = grunt.file.read path.normalize "test/expected/test2.html"
     test.equal actual, expected, "multiple inputs should concatenate"
     test.done()
 
   test3: (test) ->
     test.expect 3
-    exists = grunt.file.exists("tmp/test3.pdf")
+    exists = grunt.file.exists path.normalize "tmp/test3.pdf"
     test.ok(exists, "it should create a pdf")
 
-    actual4 = grunt.file.read("tmp/test4.html")
-    expected4 = grunt.file.read("test/expected/test4.html")
+    actual4 = grunt.file.read path.normalize "tmp/test4.html"
+    expected4 = grunt.file.read path.normalize "test/expected/test4.html"
     test.equal actual4, expected4, "it should create multiple outputs"
 
-    exists = grunt.file.exists("tmp/test5.docx")
+    exists = grunt.file.exists path.normalize "tmp/test5.docx"
     test.ok(exists, "it should create a docx")
 
     test.done()
   
   test4: (test) ->
     test.expect 2
-    exists = grunt.file.isDir("tmp/fixtures")
+    exists = grunt.file.isDir path.normalize "tmp/fixtures"
     test.ok(exists, "it should create an output directory")
 
-    actual = grunt.file.read("tmp/fixtures/input.html")
-    expected = grunt.file.read("test/expected/fixtures/input.html")
+    actual = grunt.file.read path.normalize "tmp/fixtures/input.html"
+    expected = grunt.file.read path.normalize "test/expected/fixtures/input.html"
     test.equal actual, expected, "it should create html for each input"
 
     test.done()
